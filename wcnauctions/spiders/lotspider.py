e34624c17ba8c40df7393a77d1c37660e82708e9
rignac <ignacradoslaw@gmail.com>
2021-12-06 02:43:17 +0100

    scrapy fields to arrays - prepare data for charts

--- wcnauctions/spiders/lotspider.py
+++ wcnauctions/spiders/lotspider.py
@@ -3,6 +3,18 @@ from wcnauctions.items import WcnauctionsItem
 from scrapy.loader import ItemLoader
 from datetime import datetime
 
+auctions            = []
+
+# init empty tab for plots purpose
+coinName_arr        = []
+coinCat_arr         = []
+condition_arr       = []
+price_arr           = []
+est_price_arr       = []
+bids_arr            = []
+views_arr           = []
+auctionEnd_d_arr    = []
+
 class LotSpider(scrapy.Spider):
     name = 'lot' 
     start_urls = ['https://wcn.pl/eauctions?older=1']
@@ -13,26 +25,25 @@ class LotSpider(scrapy.Spider):
             auctionNumber = auction.css('a.nicer::text').get()
             #auctions.append(auctionNumber)
             yield scrapy.Request('https://wcn.pl/eauctions/{}'.format(auctionNumber), self.parse2)
-
-    def parse2(self, response):
-        #item = WcnauctionsItem()
+            
+    def parse2(self, response): # auction page
         auction_d = response.css('div.eauction-status::text').getall()
         #auctionStart_dt = auction_dt[0]
         auctionEnd_d = auction_d[1]
         for lot in response.css('table.items tbody > tr'):
             l = ItemLoader(item = WcnauctionsItem(), selector=lot)
-
+            
             # optional info - spider_dt
             now = datetime.now()
             spider_dt = now.strftime("%d.%m.%Y %H:%M:%S")
             l.add_value('spider_dt', spider_dt)
-            
+
             coinName = lot.css('td:nth-child(2) a::text').get().split(',',maxsplit=2)[1]
             l.add_value('name', coinName)
-
+            
             coinCat = lot.css('td:nth-child(2) a::text').get().split(',',maxsplit=2)[0]
             l.add_value('category', coinCat)
-
+            
             l.add_css('condition', 'td:nth-child(3)::text')
             l.add_css('price', 'td:nth-child(4) p:first-child::text')
             l.add_css('est_price', 'td:nth-child(4) p:nth-child(2)::text')
@@ -52,7 +63,39 @@ class LotSpider(scrapy.Spider):
             l.add_value('auctionEnd_d', auctionEnd_d)
 
             yield l.load_item()
-
+            
+            # data for plots purpose
+            coinName_arr.append(coinName)
+            
+            coinCat_arr.append(coinCat)
+          
+            condition = l.get_collected_values('condition')
+            condition_arr.append(condition)
+            
+            price = l.get_collected_values('price')
+            price_arr.append(price)
+            
+            est_price = l.get_collected_values('est_price')
+            est_price_arr.append(est_price)
+            
+            bids = l.get_collected_values('bids')
+            bids_arr.append(bids)
+            
+            views = l.get_collected_values('views')
+            views_arr.append(views)
+            
+            auctionEnd_d = l.get_collected_values('auctionEnd_d')
+            auctionEnd_d_arr.append(auctionEnd_d)
+            
+            #print(coinName_arr)
+            #print(coinCat_arr)
+            #print(condition_arr)
+            #print(price_arr)
+            #print(est_price_arr)
+            #print(bids_arr)
+            #print(views_arr)
+            #print(auctionEnd_d_arr)
+            
         next_page = response.css("#content .pagination a[rel='next']").attrib['href']
         if next_page is None: 
             yield scrapy.Request(self.parse) # if there is NOT next_page go to parse(1) and scrap next auction
